e558f83138266f572d7bb1536d53f2b6a10e0946
Benjamin Day <benday@benday.com>
2021-01-05 15:27:33 -0500

    added add test method

--- after/Benday.CalculatorDemo.UnitTests/CalculatorFixture.cs
+++ after/Benday.CalculatorDemo.UnitTests/CalculatorFixture.cs
@@ -6,8 +6,37 @@ namespace Benday.CalculatorDemo.UnitTests
     public class CalculatorFixture
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add()
         {
+            // arrange
+            var systemUnderTest = new Calculator();
+            int value1 = 2;
+            int value2 = 3;
+            int expected = 5;
+
+            // act
+            int actual = systemUnderTest.Add(value1, value2);
+
+            // assert
+            Assert.AreEqual<int>(expected, actual, "wrong value");
+        }
+
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Multiply()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void Divide()
+        {
+            Assert.Inconclusive();
         }
     }
 }
