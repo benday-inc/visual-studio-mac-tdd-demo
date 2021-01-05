212938ad774d50173ca19cd22993fa576ba6fffe
Benjamin Day <benday@benday.com>
2021-01-05 15:04:21 -0500

    Created basic structure

---
+++ before/Benday.CalculatorDemo.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Benday.CalculatorDemo.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
