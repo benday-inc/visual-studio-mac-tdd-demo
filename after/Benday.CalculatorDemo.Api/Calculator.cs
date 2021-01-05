13d38d3856357f867ca6de1aca78c62cc128b5dc
Benjamin Day <benday@benday.com>
2021-01-05 16:04:54 -0500

    implement the rest of the tests

--- after/Benday.CalculatorDemo.Api/Calculator.cs
+++ after/Benday.CalculatorDemo.Api/Calculator.cs
@@ -8,6 +8,21 @@ namespace Benday.CalculatorDemo.Api
         {
             return value1 + value2;
         }
+
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        public int Divide(int value1, int value2)
+        {
+            return value1 / value2;
+        }
     }
 }
 
--- after/Benday.CalculatorDemo.UnitTests/CalculatorFixture.cs
+++ after/Benday.CalculatorDemo.UnitTests/CalculatorFixture.cs
@@ -25,19 +25,49 @@ namespace Benday.CalculatorDemo.UnitTests
         [TestMethod]
         public void Subtract()
         {
-            Assert.Inconclusive();
+            // arrange
+            var systemUnderTest = new Calculator();
+            int value1 = 2;
+            int value2 = 3;
+            int expected = -1;
+
+            // act
+            int actual = systemUnderTest.Subtract(value1, value2);
+
+            // assert
+            Assert.AreEqual<int>(expected, actual, "wrong value");
         }
 
         [TestMethod]
         public void Multiply()
         {
-            Assert.Inconclusive();
+            // arrange
+            var systemUnderTest = new Calculator();
+            int value1 = 2;
+            int value2 = 3;
+            int expected = 6;
+
+            // act
+            int actual = systemUnderTest.Multiply(value1, value2);
+
+            // assert
+            Assert.AreEqual<int>(expected, actual, "wrong value");
         }
 
         [TestMethod]
         public void Divide()
         {
-            Assert.Inconclusive();
+            // arrange
+            var systemUnderTest = new Calculator();
+            int value1 = 6;
+            int value2 = 3;
+            int expected = 2;
+
+            // act
+            int actual = systemUnderTest.Divide(value1, value2);
+
+            // assert
+            Assert.AreEqual<int>(expected, actual, "wrong value");
         }
     }
 }
